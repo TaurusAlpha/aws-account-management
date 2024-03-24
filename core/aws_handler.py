a056227fe78254a68f4be0116d4f962b683adf88
Andrey Voroshnin <96011822+TaurusAlpha@users.noreply.github.com>
2024-03-24 11:21:17 +0200

    add sc_product function

--- core/aws_handler.py
+++ core/aws_handler.py
@@ -30,9 +30,10 @@ class AWS:
             response = task(*args)
             logger.info(f"Request status: {response}")
 
-    def deregister_account(self, account_id: str) -> str:
-        provisioned_products = []
+    def __get_sc_products_for_account(self, account_id: str) -> list[str]:
+        sc_product_ids = []
         sc_client = self.aws_client_factory.get_aws_client("servicecatalog")
+
         response = sc_client.search_provisioned_products(
             AccessLevelFilter={"Key": "Account", "Value": "self"},
             Filters={"SearchQuery": [account_id]},
@@ -41,26 +42,38 @@ class AWS:
         try:
             sc_response = SearchProvisionedProductsResponse(**response)  # type: ignore[call-arg]
         except TypeError:
-            return f"Account {account_id} not found in ControlTower provisioned Service Catalog products"
-
+            return []
+        
         for sc_provisioned_product in sc_response.ProvisionedProducts:
             if sc_provisioned_product.Status != "AVAILABLE":
-                return f"Account {account_id} already terminated from ControlTower provisioned Service Catalog products"
+                continue
             else:
-                record_id_response = sc_client.terminate_provisioned_product(  # type: ignore[call-arg]
-                    ProvisionedProductId=sc_provisioned_product.Id, IgnoreErrors=True
-                )
+                sc_product_ids.append(sc_provisioned_product.Id)
+        
+        return sc_product_ids
 
-                record_id = record_id_response["RecordDetail"]["RecordId"]
+    def deregister_account(self, account_id: str) -> str:
+        provisioned_products = []
+        sc_client = self.aws_client_factory.get_aws_client("servicecatalog")
+        provisioned_products = self.__get_sc_products_for_account(account_id)
 
-                self._block_until_complete(
-                    task=lambda x: sc_client.describe_record(Id=record_id)[
-                        "RecordDetail"
-                    ],
-                    args=[],
-                    condition=lambda x: x["Status"]
-                    in ["IN_PROGRESS", "IN_PROGRESS_IN_ERROR", "CREATED"],
-                )
-            provisioned_products.append(sc_provisioned_product.Id)
+        if provisioned_products is None:
+            return f"No provisioned products found for {account_id}"
+        
+        for provisioned_product in provisioned_products:
+            record_id_response = sc_client.terminate_provisioned_product(  # type: ignore[call-arg]
+                ProvisionedProductId=provisioned_product, IgnoreErrors=True
+            )
+
+            record_id = record_id_response["RecordDetail"]["RecordId"]
+
+            self._block_until_complete(
+                task=lambda x: sc_client.describe_record(Id=record_id)[
+                    "RecordDetail"
+                ],
+                args=[],
+                condition=lambda x: x["Status"]
+                in ["IN_PROGRESS", "IN_PROGRESS_IN_ERROR", "CREATED"],
+            )
 
         return f"Succesfully terminated ControlTower account {account_id} from provisioned Service Catalog ID: {provisioned_products}"
