b1a0a647e10250daaa3f20c6a909c9e93e85a51c
Andrey <96011822+TaurusAlpha@users.noreply.github.com>
2024-03-26 20:09:46 +0200

    add docstrings (#2)

--- core/authentication/aws_client_factory.py
+++ core/authentication/aws_client_factory.py
@@ -1,3 +1,4 @@
+# mypy: disable-error-code="valid-type, name-defined"
 from __future__ import annotations
 
 import os
@@ -21,41 +22,80 @@ ClientNamePatternMatchGroup = 1
 class IAWSClientFactory(Protocol):
 
     @classmethod
-    def get[T](
+    def get[
+        T
+    ](
         cls, client_class: Type[T], region: str = ..., role_arn: Optional[str] = ...
-    ) -> T: ...
-    """
-    Only works in python > 3.12.
-    This will provide proper typing to the user regardless of boto3 vscode extension.
-    
-    Downside:
-        This requires the user to create clients using mypy_boto3 types
-    """
+    ) -> T:
+        """
+        Returns an instance of the specified AWS client class.
+
+        Args:
+            client_class (Type[T]): The class of the AWS client to create.
+            region (str, optional): The AWS region to use. Defaults to ... (provide default value).
+            role_arn (str, optional): The ARN of the role to assume. Defaults to None.
+
+        Returns:
+            T: An instance of the specified AWS client class.
+
+        Note:
+            Only works in python > 3.12.
+            This will provide proper typing to the user regardless of boto3 vscode extension.
+
+            Downside:
+                This requires the user to create clients using mypy_boto3 types
+        """
+        ...
 
 
 class AWSClientFactory:
     clients: dict[str, BaseClient] = {}
 
     @classmethod
-    def get[T](
+    def get[
+        T
+    ](
         cls,
         client_class: Type[T],
         region: str = os.environ["AWS_REGION"],
         role_arn: Optional[str] = None,
     ) -> T:
+        """
+        Get an AWS client instance.
+
+        Args:
+            client_class (Type[T]): The class of the AWS client to create.
+            region (str, optional): The AWS region to use. Defaults to the value of the AWS_REGION environment variable.
+            role_arn (str, optional): The ARN of the IAM role to assume. Defaults to None.
+
+        Returns:
+            T: An instance of the AWS client.
+
+        """
         return cls.clients.get(
             f"{client_class.__name__}_{role_arn}",
             cls.__create_aws_client(client_class, region, role_arn),
         )
 
     @classmethod
-    def __create_aws_client[T](
-        cls, client_class: Type[T], region: str, role_arn: Optional[str]
-    ) -> T:
+    def __create_aws_client[
+        T
+    ](cls, client_class: Type[T], region: str, role_arn: Optional[str]) -> T:
+        """
+        Create an AWS client of the specified type.
 
+        Args:
+            client_class (Type[T]): The type of AWS client to create.
+            region (str): The AWS region to use.
+            role_arn (Optional[str]): The ARN of the IAM role to assume (optional).
+
+        Returns:
+            T: The created AWS client.
+
+        """
         session_creds = cls.__get_sts_credentials(role_arn)
         aws_client: T = boto3.client(
-            service_name=cls.__client_type_to_service_name(client_class),
+            service_name=cls.__client_type_to_service_name(client_class),  # type: ignore
             aws_access_key_id=session_creds["AccessKeyId"],
             aws_secret_access_key=session_creds["SecretAccessKey"],
             aws_session_token=session_creds["SessionToken"],
@@ -67,17 +107,42 @@ class AWSClientFactory:
         return aws_client
 
     @staticmethod
-    def __client_type_to_service_name[T](client_class: T) -> ServiceName:
+    def __client_type_to_service_name[T](client_class: T) -> ServiceName: # type: ignore[valid-type, name-defined]
+        """
+        Converts the client type to a service name.
+
+        Args:
+            client_class (Type): The client class.
+
+        Returns:
+            ServiceName: The converted service name.
+        """
         return (
-            ClientNamePattern.match(client_class.__module__)
+            ClientNamePattern.match(client_class.__module__) # type: ignore[union-attr]
             .group(ClientNamePatternMatchGroup)
             .replace("_", "-")
         )
 
     @classmethod
     def __get_sts_credentials(cls, role_arn: Optional[str]) -> CredentialsTypeDef:
+        """
+        Get STS credentials for the specified role ARN.
+
+        Args:
+            role_arn (Optional[str]): The ARN of the role to assume.
+
+        Returns:
+            CredentialsTypeDef: The STS credentials.
+
+        If `role_arn` is None, returns a dictionary with `AccessKeyId`, `SecretAccessKey`,
+        and `SessionToken` set to None.
+
+        """
         if role_arn is None:
-            return typing.cast(CredentialsTypeDef, {k: None for k in ["AccessKeyId", "SecretAccessKey", "SessionToken"]})
+            return typing.cast(
+                CredentialsTypeDef,
+                {k: None for k in ["AccessKeyId", "SecretAccessKey", "SessionToken"]},
+            )
 
         sts_client = boto3.client("sts")
         return sts_client.assume_role(
--- core/utils/sso_admin_utils.py
+++ core/utils/sso_admin_utils.py
@@ -4,6 +4,12 @@ from core.authentication.aws_client_factory import AWSClientFactory
 
 
 def get_instance_arns() -> list[str]:
+    """
+    Retrieves the ARNs (Amazon Resource Names) of all SSO instances.
+
+    Returns:
+        A list of strings representing the ARNs of all SSO instances.
+    """
     sso_client = AWSClientFactory.get(SSOAdminClient)
     sso_response = sso_client.list_instances()
     return list(map(lambda x: x["InstanceArn"], sso_response["Instances"]))
